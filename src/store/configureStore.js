9f5770cae197c41224c481dab6b986458f552263
Xavier Perez <xap5xap@users.noreply.github.com>
2017-06-29 10:27:18 -0500

    with combineReducers

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,8 @@
 import { createStore } from 'redux';
-import  todoAppReducer  from '../reducers/todoAppReducer';
+import todoAppReducer from '../reducers/todoAppReducer';
+import rootReducer from '../reducers';
 
 export default function configureStore() {
-    return createStore(todoAppReducer);
+    // return createStore(todoAppReducer);//with reducer composition patern
+    return createStore(rootReducer);//with combinereducer function
 }
