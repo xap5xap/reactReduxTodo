0754221e7a9dbbfb328f6a9591b81440d9c9f475
Xavier Perez <xap5xap@users.noreply.github.com>
2017-06-29 10:21:18 -0500

    reducer composition pattern

--- index.ios.js
+++ index.ios.js
@@ -7,6 +7,14 @@ class todoRedux extends React.Component {
 
   store = configureStore();
 
+
+  componentDidMount() {
+    console.log(this.store.getState());
+    this.store.dispatch({ type: 'ADD_TODO', id: '2', text: 'xap'});
+    this.store.dispatch({ type: 'SET_VISIBILITY_FILTER', filter: 'SHOW_COMPLETED' });
+    console.log(this.store.getState());
+  }
+
   render() {
     return (
       <App />
